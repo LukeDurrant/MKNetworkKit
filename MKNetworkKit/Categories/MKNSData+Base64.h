4e5f01f3888af1fa34403abbc9e5423e684b2073
LukeDurrantWork <luke@gatecrasher.com.au>
2012-09-07 09:27:34 +0800

    Changed name of NSData+base64 Category to MKNSData+Base64 to remove conflict with other projects

--- MKNetworkKit/Categories/MKNSData+Base64.h
+++ MKNetworkKit/Categories/MKNSData+Base64.h
@@ -23,12 +23,12 @@
 
 #import <Foundation/Foundation.h>
 
-void *NewBase64Decode(
+void *mkNewBase64Decode(
 	const char *inputBuffer,
 	size_t length,
 	size_t *outputLength);
 
-char *NewBase64Encode(
+char *mkNewBase64Encode(
 	const void *inputBuffer,
 	size_t length,
 	bool separateLines,
@@ -36,7 +36,7 @@ char *NewBase64Encode(
 
 @interface NSData (Base64)
 
-+ (NSData *)dataFromBase64String:(NSString *)aString;
-- (NSString *)base64EncodedString;
++ (NSData *)mkDataFromBase64String:(NSString *)aString;
+- (NSString *)mkBase64EncodedString;
 
 @end
